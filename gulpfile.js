334191403f90f8027e152016e8975c285db8a99b
dominikfiala <domo.fiala@gmail.com>
2017-10-13 22:06:39 +0200

    Pridan bundle.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 
-gulp.task('bundle', function() {
+gulp.task('bundle-js', function() {
   return gulp.src([
     './node_modules/vue/dist/vue.min.js',
     './node_modules/jquery/dist/jquery.slim.min.js',
     './node_modules/popper.js/dist/umd/popper.min.js',
     './node_modules/bootstrap/dist/js/bootstrap.min.js',
-    './app.js',
+    './app.js'
   ])
   .pipe(concat('bundle.js'))
   .pipe(gulp.dest('./'));
 });
+
+gulp.task('bundle-css', function() {
+  return gulp.src([
+    './node_modules/bootstrap/dist/css/bootstrap.min.css'
+  ])
+  .pipe(concat('bundle.css'))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('bundle', ['bundle-js', 'bundle-css']);

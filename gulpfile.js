76fcb88061b53e230e54c7f5b4b27221c903b200
dominikfiala <domo.fiala@gmail.com>
2017-11-07 18:28:49 +0100

    build

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,23 @@
 
 var gulp = require('gulp');
 var electron = require('electron-connect').server.create();
+var gulpCopy = require('gulp-copy');
+var browserSync = require("browser-sync").create();
+var jeditor = require("gulp-json-editor");
+
+gulp.task("serve-browser", function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    },
+    ui: false,
+    open: false,
+    files: "./src/app.js",
+    startPath: "src/index.html"
+  });
+
+  gulp.watch("./src/index.html").on("change", browserSync.reload);
+});
 
 gulp.task('serve', function () {
   // Start browser process
@@ -13,3 +30,33 @@ gulp.task('serve', function () {
   // Reload renderer process
   gulp.watch(['src/app.js', 'src/index.html'], electron.reload);
 });
+
+gulp.task('build-manifest', function () {
+  return gulp
+    .src("app/package.json")
+    .pipe(jeditor(function(json) {
+      delete json.dependencies;
+      return json;
+    }))
+    .pipe(gulp.dest("app"));
+});
+
+gulp.task('build-copy', function () {
+  return gulp
+    .src([
+      "node_modules/bootstrap/dist/css/bootstrap.css",
+      "node_modules/jquery/dist/jquery.slim.js",
+      "node_modules/popper.js/dist/umd/popper.js",
+      "node_modules/bootstrap/dist/js/bootstrap.js",
+      "node_modules/vue/dist/vue.js",
+      "node_modules/file-saver/FileSaver.js",
+      "src/app.js",
+      "src/main.js",
+      "src/index.html",
+      "src/icon.ico",
+      "package.json"
+    ])
+    .pipe(gulpCopy('app'));
+});
+
+gulp.task('build', gulp.series('build-copy', 'build-manifest'))

1ba7fbbf9348c7fa640e2e264701ba17a2c2ea2b
dominikfiala <domo.fiala@gmail.com>
2018-02-19 12:40:18 +0100

    fix build procesu

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,6 @@ var gulp = require('gulp')
 var electron = require('electron-connect').server.create()
 var browserSync = require("browser-sync").create();
 
-gulp.task("serve-browser", function() {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    },
-    ui: false,
-    open: false,
-    files: ["./src/app.js", "./src/styles.css"],
-    startPath: "src/index.html"
-  })
-
-  gulp.watch("./src/index.html").on("change", browserSync.reload);
-})
-
 gulp.task('serve', function () {
   // Start browser process
   electron.start();
